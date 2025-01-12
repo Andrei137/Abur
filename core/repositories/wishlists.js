43f292cb8b97dd2897157b58795ee7f1d5c83263
Sebastian Mihalache <sebimih@gmail.com>
2025-01-12 18:32:17 +0200

    Wishlist Add Item

--- core/repositories/wishlists.js
+++ core/repositories/wishlists.js
@@ -6,8 +6,31 @@ import {
 import { sendError } from '@services/validation.js';
 
 const {
+    findGameById,
     findAllDLCs,
+    findWishlistItemsByFields,
+    findLibraryItemsByFields,
+    createWishlistItem
 } = requestService;
+
+export const validateAndCreateWishlistItem = async ({ gameId, customerId }) => {
+    // item NU exista in db
+    if ((await findGameById(gameId)) === null) {
+        await sendError('Select a valid item from the store');
+    }
+
+    // item NU este deja in wishlist
+    if ((await findWishlistItemsByFields(['gameId', 'customerId'], [gameId, customerId])).length !== 0) {
+        await sendError('This item is already in your wishlist');
+    }
+
+    // item NU este deja in library
+    if ((await findLibraryItemsByFields(['gameId', 'customerId'], [gameId, customerId])).length !== 0) {
+        await sendError('This item is already in your library');
+    }
+
+    return await createWishlistItem({ gameId, customerId });
+}
 
 export const getWishlistItems = async (customerId) => {
     const [ids, games, dlcs] = await Promise.all([
